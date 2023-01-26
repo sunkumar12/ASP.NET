a6318d404d8389e9d6b03ddf18dcc91ddb1871b3
sunkumar12 <123289231+sunkumar12@users.noreply.github.com>
2023-01-26 23:51:20 +0530

    Add files via upload

---
+++ ConsumeController.cs
@@ -0,0 +1,34 @@
+﻿using MynewASPWEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MynewASPWEBAPI.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        // GET: Consume
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<employee_tab1> list = new List<employee_tab1>();
+            client.BaseAddress = new Uri("https://localhost:44334/api/NewAPI");
+            var response = client.GetAsync("NewAPI");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<employee_tab1>>();
+                display.Wait();
+                list = display.Result;
+
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ NewAPIController.cs
@@ -0,0 +1,32 @@
+﻿using MynewASPWEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Mvc.Razor;
+
+namespace MynewASPWEBAPI.Controllers
+{
+    public class NewAPIController : ApiController
+    {
+        empEntities1 db = new empEntities1();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<employee_tab1> obj = db.employee_tab1.ToList();
+            return Ok(obj);
+;        }
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            var obj = db.employee_tab1.Where(model1=>model1.Empid == id).FirstOrDefault();
+            return Ok(obj);
+            
+        }
+    }
+}
